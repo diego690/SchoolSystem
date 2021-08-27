5ba71ce9cfe028984a3c7932b003eba397b60aac
diego690 <diego.capurro2016@uteq.edu.ec>
2021-08-27 01:30:13 -0500

    periodo

--- src/main/java/uteq/sga/ScchoolarSystem/controller/PeriodoControlle.java
+++ src/main/java/uteq/sga/ScchoolarSystem/controller/PeriodoControlle.java
@@ -9,6 +9,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uteq.sga.ScchoolarSystem.Entity.Periodo;
 import uteq.sga.ScchoolarSystem.Services.IPeriodoService;
@@ -30,6 +34,31 @@ public class PeriodoControlle {
         model.addAttribute("Titulo", "Listado de Periodos Academicos");
         model.addAttribute("periodos", period);
         return "/Admin/periodolist";
+    }
+    @GetMapping("createPeriodo")
+    public String crearPeriodo(Model model){
+        Periodo periodo = new Periodo();
+        model.addAttribute("Titulo", "Nuevo Periodo Lectivo");
+        model.addAttribute("periodol", periodo);
+        return "/Admin/periodoAdd";
+    }
+    @PostMapping("/addPeriodo")
+    public String guardaPeriodo(@ModelAttribute Periodo user){
+        periodoserv.guardar(user);
+        System.out.println("Periodo registrado correctamente");
+        return "redirect:/Periodo/listPeriodo";
+    }
+    @GetMapping("/editPeriodo/{id}")
+    public String editarPeriodo(@PathVariable("id") Integer iduser,Model model){
+        Periodo period= periodoserv.buscaById(iduser);
+        model.addAttribute("Titulo", "Editar Periodo Lectivo");
+        model.addAttribute("periodol", period);
+        return "/Admin/periodoAdd";
+    }
+    @GetMapping("/deletePeriodo/{id}")
+    public String deletePeriodo(@PathVariable("id") Integer id,Model model){
+        periodoserv.eliminar(id);
+        return "redirect:/Periodo/listPeriodo";
     }
     
 }
